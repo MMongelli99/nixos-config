4a64778115081e2ed101d236d9b8d6e3f0f05939
Michael Mongelli <mmongelli99@gmail.com>
2025-12-03 16:10:02 -0500

    add devenv substituters

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,13 @@
       ];
     };
 
+    # registry.custom-templates.flake = import ./flake-templates { };
+
+    extraOptions = ''
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    '';
+
   };
 
   boot = {
