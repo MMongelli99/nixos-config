0350b3c026a88e2a929a4a5d81974b71bc56489d
Michael Mongelli <mmongelli99@gmail.com>
2026-01-15 12:18:15 -0500

    wallpaper

--- users/users/mike/hm-modules/niri.nix
+++ users/users/mike/hm-modules/niri.nix
@@ -1,3 +1,6 @@
 {
   home.file.".config/niri/config.kdl".source = ../dotfiles/.config/niri/config.kdl;
+  home.file.".config/niri/wallpapers/nix-d-nord.jpg".source =
+    ../dotfiles/.config/niri/wallpapers/nix-d-nord.jpg;
+  services.swww.enable = true;
 }
