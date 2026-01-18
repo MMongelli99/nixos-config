838c5f250a08b5fdd449374b9b8ecdb25d991ec5
Michael Mongelli <mmongelli99@gmail.com>
2026-01-18 01:10:44 -0500

    add multiple wallpapers

--- users/users/mike/hm-modules/niri.nix
+++ users/users/mike/hm-modules/niri.nix
@@ -1,6 +1,8 @@
 {
   home.file.".config/niri/config.kdl".source = ../dotfiles/.config/niri/config.kdl;
-  home.file.".config/niri/wallpapers/nix-d-nord.jpg".source =
-    ../dotfiles/.config/niri/wallpapers/nix-d-nord.jpg;
+  home.file.".config/niri/wallpapers" = {
+    recursive = true;
+    source = ../dotfiles/.config/niri/wallpapers;
+  };
   services.swww.enable = true;
 }
